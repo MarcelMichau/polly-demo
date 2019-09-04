5ab07763a129f85e8720e3c408da7dfc053dcfab
Marcel Michau <Marcel.Michau@investec.co.za>
2019-09-04 07:07:18 +0200

    refactor(main): clean up call to api

--- PollyClient/Program.cs
+++ PollyClient/Program.cs
@@ -6,27 +6,29 @@ namespace PollyClient
 {
     internal class Program
     {
-        private static async Task Main(string[] args)
+        private static async Task Main()
         {
             Console.WriteLine("Starting program execution...");
-
-            var client = new HttpClient();
 
             var policy = ExponentialRetryPolicy.GetPolicy();
 
             try
             {
-                await policy.ExecuteAsync(async () =>
-                {
-                    var response = await client.GetAsync("https://localhost:5001/api/weatherforecast");
-
-                    Console.WriteLine($"Weather Forecast: {await response.Content.ReadAsStringAsync()}");
-                });
+                await policy.ExecuteAsync(CallApi);
             }
             catch (HttpRequestException)
             {
                 Console.WriteLine("Oh no! The call failed.");
             }
+        }
+
+        private static async Task CallApi()
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("https://localhost:5001/api/weatherforecast");
+
+            Console.WriteLine($"Weather Forecast: {await response.Content.ReadAsStringAsync()}");
         }
     }
 }
